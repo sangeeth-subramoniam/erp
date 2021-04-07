ba18c2fc24b39ed611979fadda8d37c880aca353
unknown <s-sangeeth-k@sicis.co.jp>
2021-04-07 16:58:07 +0900

    added admin data from local

--- erp/settings.py
+++ erp/settings.py
@@ -29,9 +29,10 @@ SECRET_KEY = 'eulb%0vr)-rafr67^d89@y93j81plb(nc3^0k!((sc%vm%6n96'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['sangeeth-erp.herokuapp.com' , '127.0.0.1' ]
+ALLOWED_HOSTS = ['*']
 
-
+import warnings
+warnings.filterwarnings("ignore", message="No directory at", module="whitenoise.base" )
 
 # Application definition
 
