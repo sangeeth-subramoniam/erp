70fd555405bbc0dae7e1e0da07aec768a9e61a29
unknown <s-sangeeth-k@sicis.co.jp>
2021-04-08 10:49:37 +0900

    mdade changes in attendance models one to one changed to Foreign key

--- attendance/models.py
+++ attendance/models.py
@@ -4,7 +4,7 @@ from structure.models import Employee
 # Create your models here.
 
 class Attendance_details(models.Model):
-    employee = models.OneToOneField(Employee, on_delete=models.CASCADE , related_name= "emp_attendance")
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE , related_name= "emp_attendance")
     year = models.CharField(max_length=4)
     month = models.CharField(max_length=10)
     date = models.CharField(max_length=2)
