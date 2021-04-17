ca5a64eb530888af6b856244faf97ccbb0ed1b55
sangeeth-subramoniam <sangeethsubramoniam@gmail.com>
2021-04-17 22:39:58 +0900

    excluded userprofile field from employee edit form in user profile

--- structure/forms.py
+++ structure/forms.py
@@ -3,12 +3,14 @@ from .models import Employee,DeptEmp
 
 
 class EmployeeUpdateForm(forms.ModelForm):
+    #user_profile = forms.CharField(disabled=True)
     emp_no = forms.IntegerField(disabled=True)
     first_name = forms.CharField(max_length=14, disabled=True)
     
     class Meta:
         model = Employee
         fields = '__all__'
+        exclude = ['user_profile',]
 
 
 
